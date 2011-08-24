349a7cf73cdf16a93c0224ee7dd7f69b95bfd1f3
Tim Nelson <tnelly27@gmail.com>
2011-08-24 11:20:09 -0500

    Added logging to Boot

--- src/main/g8/src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/g8/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,13 +1,12 @@
 package bootstrap.liftweb
 
 import net.liftweb._
-import util._
-import Helpers._
-
 import common._
 import http._
 import sitemap._
-import Loc._
+import sitemap.Loc._
+import util._
+import util.Helpers._
 
 import $package$.model._
 
@@ -16,8 +15,10 @@ import $package$.model._
  * A class that's instantiated early and run.  It allows the application
  * to modify lift's environment
  */
-class Boot {
+class Boot extends Loggable {
   def boot {
+    logger.info("Run Mode: "+Props.mode.toString)
+
     // init mongodb
     MongoConfig.init()
 
